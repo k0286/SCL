3c96e5908a4d5cdfddb4eb6655d6b57d856c0e83
sh.chen <sh.chen@g.ncu.edu.tw>
2021-03-16 11:08:50 +0800

    refactor: replace '' to NAN

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
-import pandas as pd
 import nltk
 import numpy as np
+import pandas as pd
 
-df = pd.read_csv("data/raw/train.csv", index_col='id')
-df[['POI', 'street']] = df['POI/street'].str.split('/', 1, expand=True)
-
-cond=[True if x!='' and x not in y else False for x,y in zip(df['POI'], df['raw_address'])]
+df = pd.read_csv("data/raw/train.csv", index_col="id")
+df[["POI", "street"]] = df["POI/street"].str.split("/", 1, expand=True)
+df["POI/street"].replace("/", np.nan, inplace=True)
+df["POI"].replace("", np.nan, inplace=True)
+df["street"].replace("", np.nan, inplace=True)
+cond = [True if x not in y else False for x, y in zip(df["POI"], df["raw_address"])]
 print(df[cond])
