a515f7dd3030bf4d0d0ca149ed41d52bbfc68b19
sh.chen <sh.chen@g.ncu.edu.tw>
2021-03-19 16:22:19 +0800

    feat: add NER model trainer

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
-import nltk
+import copy
+
 import numpy as np
 import pandas as pd
 
@@ -7,5 +8,81 @@ df[["POI", "street"]] = df["POI/street"].str.split("/", 1, expand=True)
 df["POI/street"].replace("/", np.nan, inplace=True)
 df["POI"].replace("", np.nan, inplace=True)
 df["street"].replace("", np.nan, inplace=True)
-cond = [True if x not in y else False for x, y in zip(df["POI"], df["raw_address"])]
-print(df[cond])
+# cond = [True if x is not np.nan and x not in y else False for x, y in zip(df["POI"], df["raw_address"])]
+# print(df[cond])
+
+df["POI_tokens"] = df["POI"].str.split()
+df["street_tokens"] = df["street"].str.split()
+df["raw_addr_tokens"] = df["raw_address"].str.replace(",", "").str.split()
+
+# for ner label
+"""
+O:
+B-P
+I-P
+E-P
+B-S
+I-S
+E-S
+"""
+# df['length'] = df['raw_address'].str.split().apply(len)
+# print(max(df['length']))  # words length
+# df['street_loc'] = [(x.find(y), len(y)) if y is not np.nan else np.nan for x,y in zip(df["raw_address"], df['street'])]  # (start, offset)
+# df['street_label']=[for x,y in zip(df["raw_address"], df['street'])]
+# label = ['O '* x for x in df['length']]
+#
+# print(df['street_loc'].head(100))
+
+
+def foo(bar: list) -> list:
+    """
+
+    :param x: raw addr
+    :param y: street
+    :return: label->list
+    """
+    label = []
+    x = bar[0]
+    y = copy.deepcopy(bar[1])
+    for i in x:
+        if y is np.nan or len(y) == 0 or i != y[0]:
+            label.append("O")
+        elif "B-S" not in label:
+            y.pop(0)
+            label.append("B-S")
+        elif len(y) == 1:
+            y.pop(0)
+            label.append("E-S")
+        else:
+            y.pop(0)
+            label.append("I-S")
+    return label
+
+
+def bar(foo: list) -> list:
+    x = foo[0]  # raw street
+    y = copy.deepcopy(foo[1])  # poi
+    z = foo[2]  # label
+    for ind in range(len(z)):
+        if y is np.nan or len(y) == 0 or x[ind] != y[0]:
+            pass
+        elif "B-P" not in z:
+            y.pop(0)
+            z[ind] = "B-P"
+        elif len(y) == 1:
+            y.pop(0)
+            z[ind] = "E-P"
+        else:
+            y.pop(0)
+            z[ind] = "I-P"
+    return z
+
+
+df["label"] = df[["raw_addr_tokens", "street_tokens"]].apply(foo, axis=1)
+df["label"] = df[["raw_addr_tokens", "POI_tokens", "label"]].apply(bar, axis=1)
+
+# drop_list=[203442, 93166, 190118, 207284, 176809, 2058, 96763]
+drop_list = [203442, 93166]
+df.drop(df.index[drop_list], inplace=True)
+df.to_csv("data/train_ner.csv")
+print("hi")
---
+++ train.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+from transformers import (
+    BertForTokenClassification,
+    BertTokenizerFast,
+    Trainer,
+    TrainingArguments,
+)
+
+df = pd.read_csv("data/train_ner.csv")
+# texts=df['raw_address'].str.split().to_list()
+texts = (
+    df["raw_addr_tokens"]
+    .str.replace("'", "")
+    .str.replace(" ", "")
+    .apply(lambda x: x[1:-1].split(","))
+    .to_list()
+)
+tags = (
+    df["label"]
+    .str.replace("'", "")
+    .str.replace(" ", "")
+    .apply(lambda x: x[1:-1].split(","))
+    .to_list()
+)
+
+train_texts, val_texts, train_tags, val_tags = train_test_split(
+    texts, tags, test_size=0.2
+)
+unique_tags = set(tag for doc in tags for tag in doc)
+tag2id = {tag: id for id, tag in enumerate(unique_tags)}
+id2tag = {id: tag for tag, id in tag2id.items()}
+
+
+tokenizer = BertTokenizerFast.from_pretrained("cahya/bert-base-indonesian-NER")
+train_encodings = tokenizer(
+    train_texts,
+    is_split_into_words=True,
+    return_offsets_mapping=True,
+    padding=True,
+    truncation=True,
+)
+val_encodings = tokenizer(
+    val_texts,
+    is_split_into_words=True,
+    return_offsets_mapping=True,
+    padding=True,
+    truncation=True,
+)
+
+
+def encode_tags(tags, encodings):
+    labels = [[tag2id[tag] for tag in doc] for doc in tags]
+    encoded_labels = []
+    for doc_labels, doc_offset, ids in zip(
+        labels, encodings.offset_mapping, encodings.input_ids
+    ):
+        # create an empty array of -100
+        doc_enc_labels = np.ones(len(doc_offset), dtype=int) * -100
+        arr_offset = np.array(doc_offset)
+
+        # set labels whose first offset position is 0 and the second is not 0
+        doc_enc_labels[(arr_offset[:, 0] == 0) & (arr_offset[:, 1] != 0)] = doc_labels
+        encoded_labels.append(doc_enc_labels.tolist())
+    return encoded_labels
+
+
+train_labels = encode_tags(train_tags, train_encodings)
+val_labels = encode_tags(val_tags, val_encodings)
+
+
+class NerDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+train_encodings.pop("offset_mapping")  # we don't want to pass this to the model
+val_encodings.pop("offset_mapping")
+
+train_dataset = NerDataset(train_encodings, train_labels)
+val_dataset = NerDataset(val_encodings, val_labels)
+
+model = BertForTokenClassification.from_pretrained("cahya/bert-base-indonesian-NER")
+
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, preds, average="binary"
+    )
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
+
+
+training_args = TrainingArguments(
+    output_dir="./results",  # output directory
+    num_train_epochs=30,  # total # of training epochs
+    per_device_train_batch_size=128,  # batch size per device during training
+    per_device_eval_batch_size=64,  # batch size for evaluation
+    warmup_steps=500,  # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,  # strength of weight decay
+    logging_dir="./logs",  # directory for storing logs
+    evaluation_strategy="epoch",
+)
+
+trainer = Trainer(
+    model=model,  # the instantiated 🤗 Transformers model to be trained
+    args=training_args,  # training arguments, defined above
+    train_dataset=train_dataset,  # training dataset
+    eval_dataset=val_dataset,  # evaluation dataset
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.evaluate()
