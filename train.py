a0fde47a9f7f71e109c67e2aa30536859bd5b1f1
sh.chen <sh.chen@g.ncu.edu.tw>
2021-03-19 16:43:14 +0800

    fix: remove f1 score metrics

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from transformers import (
     BertForTokenClassification,
@@ -98,11 +98,8 @@ model = BertForTokenClassification.from_pretrained("cahya/bert-base-indonesian-N
 def compute_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(
-        labels, preds, average="binary"
-    )
     acc = accuracy_score(labels, preds)
-    return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
+    return {"accuracy": acc}
 
 
 training_args = TrainingArguments(
